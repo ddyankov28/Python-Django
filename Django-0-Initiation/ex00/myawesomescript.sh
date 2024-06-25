0e7e448653edf35c2b13f9ebb0233d6846e6c7fa
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-06-25 11:18:54 +0200

    ex00

--- Django-0-Initiation/ex00/myawesomescript.sh
+++ Django-0-Initiation/ex00/myawesomescript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # This is the bitly shortened github location of this exercise
 # https://bit.ly/3S3Swgv
 
@@ -6,10 +7,17 @@ if [ "$#" -ne 1 ]
 then
     echo "Error: Use 1 argument (the bit.ly shortened version)"
     exit 1
+if [ curl -s $1 | wc -l > 2]
+then
+    echo "Yes"
 fi
-curl -s $1 | grep -o "http[^\"]*" 
-
+#curl -s $1 | grep -o "http[^\"]*"
+ 
 
 
 ### --- CURL ---- ###
 #   Client URL (cURL) enables data exchange between a device and a server
+
+# resources
+# - https://www.geeksforgeeks.org/curl-command-in-linux-with-examples/
+# - 
