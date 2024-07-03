e799b313c29c11c6d5a28888048eae1abd91a3b1
ddyankov28 <deyandyankov23@yahoo.com>
2024-07-03 22:49:46 +0200

    ex01

---
+++ Django-0-Starting/ex01/numbers.py
@@ -0,0 +1,11 @@
+def print_numbers():
+    numbers_file = open("numbers.txt", "r")
+    numbers = numbers_file.read()[:-1].split(",")
+    for item in numbers:
+        print(item)
+
+if __name__ == '__main__':
+    try:
+        print_numbers()
+    except Exception as e:
+        print("Error: ", e)
