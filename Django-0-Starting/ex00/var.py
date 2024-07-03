0471c81aebc65705f5e2c21568eb64d9e9ee68ff
Deyan Dyankov <deyandyankov23@yahoocom>
2024-07-03 16:26:50 +0200

    ex00

---
+++ Django-0-Starting/ex00/var.py
@@ -0,0 +1,19 @@
+def my_var():
+    list_of_vars = [ 42,
+                    "42",
+                    "quarante-deux",
+                    42.0,
+                    True,
+                    [42],
+                    {42: 42},
+                    (42,),
+                    set()]
+    
+    for item in list_of_vars:
+        print(f"{item} has a type {type(item)}")
+
+if __name__ == '__main__':
+    try:
+        my_var()
+    except Exception as e:
+        print("Error: ", e)
