e990ae07044290d9d92b977b6e49d34b329bb401
ddyankov <deyandyankov23@yahoo.com>
2024-07-04 12:11:12 +0200

    ex04

--- Django-0-Starting/ex04/state.py
+++ Django-0-Starting/ex04/state.py
@@ -1,1 +1,33 @@
 import sys
+
+
+def find_state():
+    if len(sys.argv) != 2:
+        return
+    states = {
+        "Oregon" : "OR",
+        "Alabama" : "AL",
+        "New Jersey": "NJ",
+        "Colorado" : "CO"
+    }
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+    if sys.argv[1] not in capital_cities.values():
+        print("Unknown capital city")
+        return
+    for abrreviation, capital in capital_cities.items():
+        if sys.argv[1] == capital:
+            for state, abbr in states.items():
+                if abbr == abrreviation:
+                    print(state)
+
+
+if __name__ == '__main__':
+    try:
+        find_state()
+    except Exception as e:
+        print("Error: ", e)
