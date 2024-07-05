0e4376421f8618aa9776069979d57f01170a321c
ddyankov <deyandyankov23@yahoo.com>
2024-07-05 11:05:35 +0200

    ex05

--- Django-0-Starting/ex05/all_in.py
+++ Django-0-Starting/ex05/all_in.py
@@ -2,12 +2,25 @@ import sys
 
 def find_state(item, states, capital_cities):
     for abrreviation, capital in capital_cities.items():
-        if item == capital:
+        if item.lower() == capital.lower():
             for state, abbr in states.items():
                 if abbr == abrreviation:
                     return state
 
+def find_if_capital(states, capital_cities, item):
+    for state in states.keys():
+        if item.lower() == state.lower():
+            print(f"{capital_cities.get(states.get(state))} is the capital of {state}")
+            return True
+    return False
 
+def find_if_state(states, capital_cities, item):
+    for capital in capital_cities.values():
+        if item.lower() == capital.lower():
+            state_var = find_state(item, states, capital_cities)
+            print(f"{capital} is the capital of {state_var}")
+            return True
+    return False
 
 def all_in():
     if len(sys.argv) != 2:
@@ -24,26 +37,17 @@ def all_in():
         "NJ": "Trenton",
         "CO": "Denver"
     }
-    list_args = sys.argv[1].split(",")
-    for item in list_args:
-        for state in states.keys():
-            # print(f"This is the input {item.lower()}")
-            # print(f"This is the state {state.lower()}")
-            if item.lower() == state.lower():
-                print(f"{capital_cities.get(states.get(state))} is the capital of {state}")
-                continue
-            for capital in capital_cities.values():
-                if item.lower() == capital.lower():
-                    state_var = find_state(item, states, capital_cities)
-                    print(f"{capital} is the capital of {state_var}")
+    for item in sys.argv[1].split(","):
+        item = item.strip()
+        if not item:
+            continue
+        if find_if_capital(states, capital_cities, item):
+            continue
+        if find_if_state(states, capital_cities, item):
+            continue
+        print(f"{item} is neither a capital city nor a state")
                     
                                
-        
-
-        
-
-
-
 if __name__ == '__main__':
     try:
         all_in()
