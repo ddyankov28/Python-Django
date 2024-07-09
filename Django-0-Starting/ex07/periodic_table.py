731f0b926e2b0d3ad9e666e0b04b932262be12c5
ddyankov28 <deyandyankov23@yahoo.com>
2024-07-09 16:49:20 +0200

    ex07

--- Django-0-Starting/ex07/periodic_table.py
+++ Django-0-Starting/ex07/periodic_table.py
@@ -5,10 +5,41 @@ def periodic_table():
     print("This is the table")
     file = open('periodic_table.txt', 'r')
     elements = file.read().split('\n')
-    print(elements[0])
-
-
-    html_content = """
+    table_rows = "<tr>"
+    current_position = 0
+    for element in elements:
+        if element.strip():
+            components = element.split(',')
+            position = int(components[0].split(':')[1].strip())
+            name = components[0].split('=')[0].strip()
+            atomic_number = components[1].split(':')[1].strip()
+            symbol = components[2].split(':')[1].strip()
+            atomic_mass = components[3].split(':')[1].strip()
+            print(f"Position: {position}")
+            print(f"Current position: {current_position}")
+        
+            if position == 0 and current_position != 0:
+                table_rows += "</tr><tr>"
+            
+            while current_position < position:
+                table_rows += "<td></td>"
+                current_position += 1
+                
+            table_rows += f"""
+            <td>
+                <h4>{name}</h4>
+                <ul>
+                    <li>No {atomic_number}</li>
+                    <li>{symbol}</li>
+                    <li>{atomic_mass}</li>
+                </ul>
+            </td>
+            """
+        current_position = position + 1
+    
+    table_rows += "</tr>"
+    
+    html_content = f"""
     <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -17,16 +48,7 @@ def periodic_table():
         </head>
         <body>
             <table>
-                <tr>
-                    <td>
-                        <h4>Hydrogen</h4>
-                        <ul>
-                            <li>No 42</li>
-                            <li>H</li>
-                            <li>1.00794</li>
-                        <ul>
-                    </td>
-                <tr>
+                {table_rows}
             </table>
         <body>
     </html>
@@ -35,8 +57,20 @@ def periodic_table():
     html_file.write(html_content)
 
     css_content="""
+    table {
+        border-collapse: collapse;
+    }
     td {
         border: 3px solid;
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        vertical-align: middle;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0
     }
     """
     css_file = open("style.css", 'w')
