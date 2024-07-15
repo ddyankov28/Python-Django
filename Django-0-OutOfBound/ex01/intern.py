d4d6b8a51b8877b30091b8a8789912786e86fb4c
ddyankov <deyandyankov23@yahoo.com>
2024-07-15 13:58:22 +0200

    ex01

---
+++ Django-0-OutOfBound/ex01/intern.py
@@ -0,0 +1,32 @@
+class Intern:
+    def __init__(self, name="My name? I'm nobody, an intern, I have no name."):
+        self.name = name
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+    class Coffee:
+        def __str__(self):
+            return "This is the worst coffee you ever tasted"
+
+    
+    def work(self):
+        raise Exception("I'm just an intern, I can't do that...")
+    
+    
+    def make_coffee(self):
+        return self.Coffee()
+
+    
+if __name__ == '__main__':
+    try:
+        intern = Intern()
+        mark = Intern("Mark")
+        print(intern)
+        print(mark)
+        print(mark.make_coffee())
+        intern.work()
+    except Exception as e:
+        print("Error: ", e)
