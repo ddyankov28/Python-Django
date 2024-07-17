f9183303ef1a979a760bc7caa05392662bc908bb
ddyankov <deyandyankov23@yahoo.com>
2024-07-17 14:04:01 +0200

    ex04

--- Django-0-OutOfBound/ex04/elem.py
+++ Django-0-OutOfBound/ex04/elem.py
@@ -12,14 +12,19 @@ class Text(str):
         """
         Do you really need a comment to understand this method?..
         """
-        return super().__str__().replace('\n', '\n<br />\n')
+        replace_char_dict = {"<": "&lt", ">": "&rt", '"': "&quot", "\n": '\n<br />\n'}
+        for key, value in replace_char_dict.items():
+            text = super().__str__().replace(key, value)
+        return text
 
 
 class Elem:
     """
     Elem will permit us to represent our HTML elements.
     """
-    [...]
+    class ValidationError(Exception):
+        def __init__(self, err_message="Element Validation Error"):
+            super().__init__(err_message)
 
     def __init__(self, tag='div', attr={}, content=None, tag_type='double'):
         """
@@ -29,8 +34,14 @@ class Elem:
         """
         self.tag = tag
         self.attr = attr
-        self.content = content
+        self.content = []
+        if type(content) == list:
+            self.content = content
+        elif content is not None:
+                self.content.append(content)
         self.tag_type = tag_type
+        if tag_type != "double" and tag_type != "simple":
+            raise self.ValidationError("Wrong Element Tag Type")
 
     def __str__(self):
         """
@@ -40,11 +51,9 @@ class Elem:
         elements...).
         """
         if self.tag_type == 'double':
-            result = ""
-            result += f"<{self.tag}></{self.tag}>"
+            result = f"<{self.tag}{self.__make_attr()}>{self.__make_content()}</{self.tag}>"
         elif self.tag_type == 'simple':
-            result = """"""
-            result += f"""<{self.tag}"""
+            result = f"<{self.tag}{self.__make_attr} />"
         return result
 
     def __make_attr(self):
@@ -60,17 +69,19 @@ class Elem:
         """
         Here is a method to render the content, including embedded elements.
         """
-
+        
         if len(self.content) == 0:
             return ''
         result = '\n'
         for elem in self.content:
-            result += [...]
+            if (len(str(elem))):
+                result += f"{elem}\n"
+        result = " ".join(line for line in result.splitlines(True))
         return result
 
     def add_content(self, content):
         if not Elem.check_type(content):
-            raise Elem.ValidationError
+            raise Elem.ValidationError("Not right type")
         if type(content) == list:
             self.content += [elem for elem in content if elem != Text('')]
         elif content != Text(''):
@@ -89,8 +100,12 @@ class Elem:
 
 
 if __name__ == '__main__':
-    page = Elem('html', content=[
-                Elem('head', content=Elem('title', content=Text('"Hello ground!"'))),
-                Elem('body', content=[Elem('h1', content=Text('"Oh no, not again!"')),
-                Elem('img', {'src': 'http://i.imgur.com/pfp3T.jpg'}, tag_type='simple')])])
-    print(page)
+    try:
+        html_page = Elem("html", {"lang": "en"}, content=[
+                        Elem("head", content=[
+                            Elem("title", content="\"Hello ground!\"")
+                        ])
+        ])
+        print(html_page)
+    except Exception as e:
+        print("Error: ", e)
