7c72b35ff944bbd06b09b7499657228149c2f954
ddyankov <deyandyankov23@yahoo.com>
2024-08-02 10:21:16 +0200

    ex01

--- Django-1-Libraries/ex01/my_program.py
+++ Django-1-Libraries/ex01/my_program.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from path import Path
 
-new_folder = Path('new_folder_from_path')
+new_folder = Path('new_folder')
 new_folder.makedirs_p()
-file = open('new_folder_from_path/new_file', 'w')
+file = open('new_folder/new_file', 'w')
 file.write("This is the file content")
 file.close()
-file = open('new_folder_from_path/new_file', 'r')
+file = open('new_folder/new_file', 'r')
 print(file.read())
--- Django-1-Libraries/ex01/my_script.sh
+++ Django-1-Libraries/ex01/my_script.sh
@@ -14,13 +14,16 @@ cat << EOF > my_program.py
 #!/usr/bin/env python3
 from path import Path
 
-new_folder = Path('new_folder_from_path')
+new_folder = Path('new_folder')
 new_folder.makedirs_p()
-file = open('new_folder_from_path/new_file', 'w')
+file = open('new_folder/new_file', 'w')
 file.write("This is the file content")
 file.close()
-file = open('new_folder_from_path/new_file', 'r')
+file = open('new_folder/new_file', 'r')
 print(file.read())
 EOF
 
 python3 my_program.py
+
+#rm -rf new_folder
+#rm -rf local_lib
