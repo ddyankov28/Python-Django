a73eba1d291455d3a1ed82a808dd8a979489e060
ddyankov <deyandyankov23@yahoo.com>
2024-10-16 12:40:51 +0200

    wiki

--- Django-1-Libraries/ex02/request_wikipedia.py
+++ Django-1-Libraries/ex02/request_wikipedia.py
@@ -10,20 +10,27 @@ def main():
     assert sys.argv[1].strip(), "Wrong/Empty Parameter"
     search_term = sys.argv[1]
     params = {
-        'action'     : 'parse',
-        'page'       : search_term,
-        'format'     : 'json',
-        'redirects'  : True,
+        'action'     : 'query',
+        'titles'     : search_term,
+        'prop'     : 'revisions',
+        'rvprop'  : 'content',
+        'format'  : 'json',
+        'redirects' : True
     }
     response = requests.get(url, params=params)
-    print(response.url)
+    print("Response URL: ", response.url)
     assert response, f"{response.raise_for_status()}"
     data = response.json()
-    #print(data)
-    page_text = data.get('parse').get('text').get('*')
-    print(page_text
-          )
+    page = next(iter(data['query']['pages'].values()))
+    assert 'revisions' in page, "No info for this search term"
+    page_text = page['revisions'][0]['*']
+    plain_text = dewiki.from_string(page_text).strip()
+    file = open(f"{search_term}.wiki", "w")
+    file.write(plain_text)
+    file.close()
 
+    # plain_text = dewiki.from_string(page_text)
+    # print(plain_text)
 if __name__ == "__main__":
     try:
         main()        
