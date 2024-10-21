f70ee791a647e50b598b4143c85cd95ee7f503a3
ddyankov <deyandyankov23@yahoo.com>
2024-10-21 16:17:55 +0200

    forms

--- Django-1-BaseDjango/ex02/d05/d05/settings.py
+++ Django-1-BaseDjango/ex02/d05/d05/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOG_FILE_PATH = Path(BASE_DIR) / 'ex02' / 'logs.txt'
--- Django-1-BaseDjango/ex02/d05/ex02/views.py
+++ Django-1-BaseDjango/ex02/d05/ex02/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from pathlib import Path
+from django.conf import settings
+from .forms import InputForm
+from datetime import datetime
 
-# Create your views here.
+
+LOG_FILE_PATH = Path(settings.BASE_DIR) / 'ex02' / 'logs.txt'
+
+def index(request):
+    history = []
+    form = InputForm()
+    return render(request, 'forms.html', {'form': form, 'history': history})
+
